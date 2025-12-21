fe291f778456fe7dc0220be639c123eed5e216f0
Bhupesh-aher <aherbhupesh18@gmail.com>
2025-12-21 23:40:04 +0530

    Buy and Sell Stocks II | Tabulation | Space optimization

--- L36/bestTimeToBuyStocks_II.cpp
+++ L36/bestTimeToBuyStocks_II.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 
+
+// You can buy an sell as many times as you want 
+
 // if buy == 1 means you can buy the stock on that index 
 // if buy == 0 means you have already bought the stock now you have 2 choices either sell it or don't sell it 
 // and if you sell than in next index you can buy again
@@ -63,6 +66,69 @@ int maxProfit1(vector<int>& prices) {
 
 
 
+
+
+// Tabulation 
+
+int maxProfit2(vector<int>& prices) {
+    int n = prices.size();
+    vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+    dp[n][0] = 0;
+    dp[n][1] = 0;
+
+    for(int ind = n -1; ind >= 0; ind--){
+        for(int buy = 0; buy <=1; buy ++){
+            int profit = 0;
+                if(buy == 1){                  
+                    profit = max(-prices[ind] +dp[ind + 1][0], 0 + dp[ind + 1][1]);
+        
+                }
+                else {
+                    profit = max(prices[ind] + dp[ind+1][1], 0 + dp[ind + 1][0]);
+                }           
+                dp[ind][buy] = profit;
+        }
+    }
+
+    return dp[0][1];
+}
+
+// TC - O(N * 2) 
+// SC - O(N * 2)      dp
+
+
+
+
+// Space optimization
+
+int maxProfit3(vector<int>& prices) {
+    int n = prices.size();
+    vector<int> prev(2, 0), curr(2, 0);
+
+    prev[0] = 0;
+    prev[1] = 0;
+
+    for(int ind = n -1; ind >= 0; ind--){
+        for(int buy = 0; buy <=1; buy ++){
+            int profit = 0;
+                if(buy == 1){                  
+                    profit = max(-prices[ind] + prev[0], 0 + prev[1]);
+        
+                }
+                else {
+                    profit = max(prices[ind] + prev[1], 0 + prev[0]);
+                }           
+                curr[buy] = profit;
+        }
+        prev = curr;
+    }
+
+    return prev[1];
+}
+
+// TC - O(N * 2) 
+// SC - O (4)     prev and curr
 
 
 
